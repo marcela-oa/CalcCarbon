4a0659909f7421972e5e3bb676d231a247430588
ViniciusNeves12 <viniciusneves1312@hotmail.com>
2022-03-09 15:29:31 -0300

    Criando a classe cadastro

--- src/br/com/calcCarbon/projeto/CalcCarbon.java
+++ src/br/com/calcCarbon/projeto/CalcCarbon.java
@@ -1,9 +1,14 @@
 package br.com.calcCarbon.projeto;
 
 public class CalcCarbon {
+	
 
-	public static void main(String[] args) {
-		//classe main, onde vai rodar o programa
+public static void main(String[] args) {
+		
+	Cadastro usuario = new Cadastro();
+	
+		Cadastro.bemVindo();
+		Cadastro.scanner();
 	}
 
 }
